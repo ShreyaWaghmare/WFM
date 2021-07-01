a1508941cb9a15943a10466ada611339a3f7cf5b
ShreyaWaghmare <70474980+ShreyaWaghmare@users.noreply.github.com>
2021-07-01 13:05:59 +0530

    Update settings.py

--- wildandfreemind/settings.py
+++ wildandfreemind/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'wfm_db',
         'USER':'postgres',
-        'PASSWORD':'@Bcd1234',
+        'PASSWORD':'postgres',
         'HOST':'localhost',
         'PORT': '5432'
     }
